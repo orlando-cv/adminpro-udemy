0c78c1fbfa30d9732480ac976f57ed6188049892
Orlando Campos <ocampos.vazquez@gmail.com>
2020-03-06 14:36:40 -0600

    Sección 8 terminada

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -13,6 +13,8 @@ import { PAGES_ROUTES } from './pages.routes';
 import { IncrementadorComponent } from '../components/incrementador/incrementador.component';
 import { GraficoDonaComponent } from '../components/grafico-dona/grafico-dona.component';
 import { AccountSettingsComponent } from './account-settings/account-settings.component';
+import { PromesasComponent } from './promesas/promesas.component';
+import { RxjsComponent } from './rxjs/rxjs.component';
 
 // TEMPORAL
 
@@ -26,7 +28,9 @@ import { AccountSettingsComponent } from './account-settings/account-settings.co
         Graficas1Component,
         IncrementadorComponent,
         GraficoDonaComponent,
-        AccountSettingsComponent
+        AccountSettingsComponent,
+        PromesasComponent,
+        RxjsComponent
     ],
     exports: [
         PagesComponent,
--- src/app/pages/pages.routes.ts
+++ src/app/pages/pages.routes.ts
@@ -3,6 +3,8 @@ import { PagesComponent } from './pages.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProgressComponent } from './progress/progress.component';
 import { Graficas1Component } from './graficas1/graficas1.component';
+import { PromesasComponent } from './promesas/promesas.component';
+import { RxjsComponent } from './rxjs/rxjs.component';
 import { AccountSettingsComponent } from './account-settings/account-settings.component';
 
 
@@ -10,10 +12,12 @@ const pagesRoutes: Routes = [
     { path: '',
     component: PagesComponent,
     children: [
-        { path: 'dashboard', component: DashboardComponent },
-        { path: 'progress', component: ProgressComponent },
-        { path: 'graficas1', component: Graficas1Component },
-        { path: 'account-settings', component: AccountSettingsComponent },
+        { path: 'dashboard', component: DashboardComponent, data: { titulo: 'Dahsboard' } },
+        { path: 'progress', component: ProgressComponent, data: { titulo: 'Progress' } },
+        { path: 'graficas1', component: Graficas1Component, data: { titulo: 'Gráficas' } },
+        { path: 'promesas', component: PromesasComponent, data: { titulo: 'Promesas' } },
+        { path: 'rxjs', component: RxjsComponent, data: { titulo: 'RxJs' } },
+        { path: 'account-settings', component: AccountSettingsComponent, data: { titulo: 'Ajustes de Tema' } },
         { path: '', redirectTo: '/dashboard', pathMatch: 'full' }
 
     ] }
